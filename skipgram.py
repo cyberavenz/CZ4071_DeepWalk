e1cfb2e438bb87837141b47212fc52fac9503188
DeonT12 <35946281+DeonT12@users.noreply.github.com>
2019-04-13 14:50:23 +0800

    Initial Skipgram algo

---
+++ skipgram.py
@@ -0,0 +1,30 @@
+#official
+
+from collections import Counter, Mapping
+from concurrent.futures import ProcessPoolExecutor
+import logging
+from multiprocessing import cpu_count
+from six import string_types
+
+from gensim.models import Word2Vec
+from gensim.models.word2vec import Vocab
+
+logger = logging.getLogger("deepwalk")
+
+class Skipgram(Word2Vec):
+    """A subclass to allow more customization of the Word2Vec internals."""
+
+    def __init__(self, words_count = None, **kwargs):
+
+        self.words_count = None
+
+        kwargs["min_count"] = kwargs.get("min_count", 0)
+        kwargs["workers"] = kwargs.get("workers", cpu_count())
+        kwargs["size"] = kwargs.get("size", 128)
+        kwargs["sentences"] = kwargs.get("sentences", None)
+        kwargs["window"] = kwargs.get("window", 10)
+		 
+			if words_count != None:
+			self.words_count = words_count
+
+        super(Skipgram, self).__init__(**kwargs)
